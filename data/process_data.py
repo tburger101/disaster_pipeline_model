3e89ca672b19bc1581d10cc6eb907f38991afb4d
root <travis.rothlisberger@gmail.com>
2020-03-01 18:52:01 +0000

    Initial commit for ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,52 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    '''This method loads both the messages and categories DF, merges them together, and breaks up the
+    messages' cateogry into 36 different binary classes to be able to be used for a multioutput classifier
+    '''
+    
+    #Loading the data sets
+    categories = pd.read_csv(categories_filepath)
+    categories.set_index('id', inplace=True)
+    
+    messages =  pd.read_csv(messages_filepath)
+    messages.set_index('id', inplace=True)
+    
+    #Merging the Datasets
+    df = pd.merge(messages, categories, left_index=True, right_index=True)
+   
+    return(df)
 
 def clean_data(df):
-    pass
+     
+    #Splitting the category column into 36 different columns
+    categories_split = df['categories'].str.split(";", expand=True)
+    row = categories_split.iloc[0].values
+    category_colnames = [x[:-2] for x in row]
+    categories_split.columns = category_colnames
+    
+    #Changing the value of the categories to be binary
+    for column in categories_split:
+    # set each value to be the last character of the string
+        categories_split[column] =categories_split[column].apply(lambda x : x[-1])    
+    # convert column from string to numeric
+        categories_split[column] =categories_split[column].apply(lambda x : int(x))
+    
 
+    #Merging the two dataframes together and drop duplicate column
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.merge(df, categories_split, left_index=True, right_index=True)
+    
+    #Removing duplicates
+    df.drop_duplicates(inplace=True)
+    return(df)
 
 def save_data(df, database_filename):
-    pass  
+    engine_text="sqlite:///"+str(database_filename)
+    engine = create_engine(engine_text)
+    df.to_sql('messages', engine, index=False)  
 
 
 def main():
